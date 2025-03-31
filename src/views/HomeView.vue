8b5b283d476e53cb3315f87921b679f220cf2795
Nelson Neto <82171194+nelsul@users.noreply.github.com>
2025-03-30 22:22:20 -0400

    feat: create home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="card">
+    <div class="card-body text-center">
+      <h4>Welcome to the Service Order Creator</h4>
+
+      <div></div>
+
+      <span
+        >Please, <RouterLink to="/">Login</RouterLink> (or
+        <RouterLink to="/">Register</RouterLink>)</span
+      >
+    </div>
+  </div>
 </template>
